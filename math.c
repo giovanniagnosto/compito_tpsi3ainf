e4f7c8564611115fbab8bde65c42cd7793d1ebcc
giovanniagnosto <giovanniagnosto8@gmail.com>
2024-12-02 08:59:57 +0100

    Add files via upload

---
+++ math.c
@@ -0,0 +1,42 @@
+math.c
+#include <stdio.h>
+#include hello.c 
+int void;
+
+{
+    int num1
+    int num2
+    int somma
+    int differenza
+    int divisione
+    int moltiplicazione
+
+}
+
+{
+    printf ("inserisci il primo numero");
+    scanf (num1);
+    printf ("inserisci il secondo numero");
+    scanf (num2);
+
+
+
+}
+return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
